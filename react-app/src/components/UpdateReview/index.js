1b0b1864bacd6df5ecd0b4c72dc0810c174e9829
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2022-12-04 21:55:12 -0800

    Add history push to move logged out users to homepage

--- react-app/src/components/UpdateReview/index.js
+++ react-app/src/components/UpdateReview/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useHistory, useParams } from 'react-router-dom';
+import { NavLink, Redirect, useHistory, useParams } from 'react-router-dom';
 import { updateReview } from '../../store/review';
 import { loadOneReview } from '../../store/review';
 import CategoriesNavBar from '../CategoriesNavBar';
@@ -14,7 +14,8 @@ const UpdateReviewForm = () => {
 	// console.log('review id 18', reviewId);
 	const history = useHistory();
 	const [loaded, setLoaded] = useState(false);
-
+	const [renderErr, setRenderErr] = useState(false);
+	const [reviewErr, setReviewErr] = useState('')
 	// const userReviews = useSelector(state => state.reviews.userReviews)
 	const currentProd = useSelector((state) => state.products.oneProduct);
 	const prodReview = useSelector((state) =>
@@ -23,12 +24,15 @@ const UpdateReviewForm = () => {
 	const user = useSelector((state) => state.session.user);
 	// console.log('find user >>>>>>>>', findUser)
 	const findUser = prodReview.find((id) => id.user_id.id === user.id);
+
 	const [review_msg, setReview_msg] = useState(findUser.review_msg);
+	const [updatedMsg, setUpdatedMsg] = useState('')
 	// console.log('find user >>>>', findUser);
 	// console.log('prod reviews >>>', prodReview);
 	// console.log('user reviews', userReviews )
 	// console.log('user >>>>', user);
 	// console.log('current product >>>>>>>', currentProd);
+
 
 	useEffect(() => {
 		// console.log('review id in dispatch', reviewId)
@@ -36,28 +40,47 @@ const UpdateReviewForm = () => {
 	}, [dispatch]);
 	// helper functions
 	const updateReviewMsg = (e) => {
-		setReview_msg(e.target.value);
+		if(review_msg.length !== updateReviewMsg.length){
+			setUpdatedMsg(e.target.value);
+		}
 	};
 
 	let product_image;
 	if (currentProd.product_photos) {
 		product_image = currentProd.product_photos[0].prod_photo;
 	}
-	// useEffect(()=>{
-	//   if(prodReview){
-	//     setLoadedProductId(true)
-	//   }
-	// })
+
 	const handleUpdateReview = async (e) => {
 		e.preventDefault();
 
 		const payload = {
-			review_msg,
+			review_msg:updatedMsg,
 			reviewId,
 		};
 		const data = await dispatch(updateReview(payload));
 		history.push(`/products/${currentProd.id}`);
+
+		if(updatedMsg){
+			setReviewErr(updatedMsg)
+		}
 	};
+
+	useEffect(() =>{
+		if(!review_msg.trim().length){
+			setReviewErr('review cannot be empty.')
+		}
+		else if(review_msg.trim().length > 255){
+			setReviewErr('Review must be less than 255 characters')
+		} else{
+			setReviewErr('')
+		}
+	},[review_msg])
+
+	if(!user){
+		return(
+			<Redirect to='/'/>
+		)
+	}
 	return (
 		<>
 			{loaded && (
@@ -79,12 +102,17 @@ const UpdateReviewForm = () => {
 										className='review-form-container'
 										onSubmit={handleUpdateReview}
 									>
-										<label
+										{reviewErr ?(<label
+											className='review-header font-16'
+											style={{ fontWeight: 'bold' }}
+										>
+											Review: {renderErr}
+										</label>):(<label
 											className='review-header font-16'
 											style={{ fontWeight: 'bold' }}
 										>
 											Review
-										</label>
+										</label>)}
 										<input
 											className='review-input font-14'
 											type='text'
@@ -95,6 +123,7 @@ const UpdateReviewForm = () => {
 											<button
 												className='update-submit-button font-16-white'
 												type='submit'
+												onSubmit={handleUpdateReview}
 											>
 												update your review
 											</button>
