5f8e234c7e9bd8fbf27d51b099d02918d83c2862
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-01-18 23:48:32 -0800

    Fix css on navbar buttons and profile dropdown menu

--- react-app/src/components/BasketDropDown/basketDropDown.js
+++ react-app/src/components/BasketDropDown/basketDropDown.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import {
+	loadUserCart,
+	increaseItem,
+	decrementItem,
+} from '../../store/shoppingCarts';
+import { addToPurchaseHistory } from '../../store/purchaseHistories';
+import { allProducts } from '../../store/products';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import './basketDropDown.css';
 
-const BasketDropDown = () =>{
+const BasketDropDown = () => {
+	const history = useHistory();
+	const dispatch = useDispatch();
+	// obj of userbasket
+	const userBasket = useSelector((state) => state.basket.userBasket);
 
-}
+	const products = useSelector((state) => state.products.allProducts);
 
-export default BasketDropDown
+	// array of user items
+	// const items = userBasket.cart_prod ? Object.values(userBasket.cart_prod) : [];
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(allProducts())
+      await dispatch(loadUserCart(userId))
+      
+    })
+  })
+};
+
+export default BasketDropDown;
