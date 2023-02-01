f3efd9d927e84bad17329980b24b4741fdf7d2d2
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-01-31 20:51:14 -0800

    Add search bar

---
+++ react-app/src/components/SearchBar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const SearchBar = () =>{
+
+  return(
+    <form>
+      <input
+      name="search">
+
+      </input>
+    </form>
+  )
+}
+
+export default SearchBar
