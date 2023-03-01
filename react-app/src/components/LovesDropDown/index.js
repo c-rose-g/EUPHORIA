9c7cdef726b52b3c9cfcfeeb464cb21122238870
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-02-28 21:36:16 -0800

    Fix love dropdown

--- react-app/src/components/LovesDropDown/index.js
+++ react-app/src/components/LovesDropDown/index.js
@@ -1,49 +1,40 @@
 import React, { useState, useSelector } from 'react';
 import { useDispatch } from 'react-redux';
 import { BsSuitHeart, BsSuitHeartFill } from 'react-icons/bs';
-
+// import { BsMoonStarsFill, BsMoonStars } from "react-icons/bs";
 import './index.css';
 const LovesDropDown = () => {
 	const dispatch = useDispatch();
 	// const loves = useSelector((state) => state.loves.loves);
 	const [dropdown, setDropdown] = useState(false);
 	// const userloves = useSelector((state) => Object.values(state.loves.loves));
-
+	// NOTE vvv why tf is this not working vvv
 	// const SignUser = useSelector(state => state.session.user);
 	// console.log(SignUser)
-
+// <div className='loves-dropdown-heart'><BsSuitHeartFill /></div>
 	return (
-		// <>
 		<div
 			className='loves-dropdown-container'
 			onMouseOver={() => setDropdown(true)}
 			onMouseLeave={() => setDropdown(false)}
-			// style={{ backgroundColor: 'white' }}
 		>
-			<button
-				className='loves-dropdown-button'
-				// style={{ backgroundColor: 'white'}}
-			>
-				{dropdown ? (
-					<div className='loves-dropdown-heart'>
-						<BsSuitHeartFill />
-					</div>
-				) : (
-					<BsSuitHeart />
-				)}
+			<button className='loves-dropdown-button'>
+        <div>
+				{dropdown ? (<BsSuitHeartFill />) : (<BsSuitHeart />)}
+        {/* <img src={dropdown ? (<BsMoonStarsFill />) : (<BsMoonStars />) } /> */}
+        </div>
 			</button>
-			{/* <div className='loves-dropdown-list' style={{transition: 'all .2s linear 0s'}}>list goes here</div> */}
-			{/* style={{  transition: 'color 0.2s ease 0s'}} */}
+
 			{dropdown && (
 				<div
 					className='loves-dropdown-page'
-					style={{ transition: 'all .8s linear 0s' }}
+					// style={{ transition: 'all .8s linear 0s' }}
 				>
 					{/* {SignUser ? '    list goes here' : '   need to sign in'} */}
+          list goes here
 				</div>
 			)}
 		</div>
-		// </>
 	);
 };
 
