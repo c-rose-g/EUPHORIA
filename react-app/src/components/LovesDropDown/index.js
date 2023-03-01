d1d024dd414ae782ca4986049a20f36fd9370edc
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-03-01 12:29:04 -0800

    Fix import error in love drop down

--- react-app/src/components/LovesDropDown/index.js
+++ react-app/src/components/LovesDropDown/index.js
@@ -1,37 +1,36 @@
-import React, { useState, useSelector } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BsSuitHeart, BsSuitHeartFill } from 'react-icons/bs';
-// import { BsMoonStarsFill, BsMoonStars } from "react-icons/bs";
+
 import './index.css';
 const LovesDropDown = () => {
 	const dispatch = useDispatch();
-	// const loves = useSelector((state) => state.loves.loves);
+	const loves = useSelector(state => state.loves.loves);
+  console.log('this is loves', loves)
 	const [dropdown, setDropdown] = useState(false);
 	// const userloves = useSelector((state) => Object.values(state.loves.loves));
 	// NOTE vvv why tf is this not working vvv
-	// const SignUser = useSelector(state => state.session.user);
+	const SignUser = useSelector(state => state.session.user);
 	// console.log(SignUser)
 // <div className='loves-dropdown-heart'><BsSuitHeartFill /></div>
 	return (
 		<div
 			className='loves-dropdown-container'
 			onMouseOver={() => setDropdown(true)}
-			onMouseLeave={() => setDropdown(false)}
+			onMouseLeave={() => setDropdown(true)}
 		>
 			<button className='loves-dropdown-button'>
-        <div>
+
 				{dropdown ? (<BsSuitHeartFill />) : (<BsSuitHeart />)}
-        {/* <img src={dropdown ? (<BsMoonStarsFill />) : (<BsMoonStars />) } /> */}
-        </div>
+
 			</button>
 
 			{dropdown && (
 				<div
 					className='loves-dropdown-page'
-					// style={{ transition: 'all .8s linear 0s' }}
+					style={{ transition: 'all .8s linear 0s' }}
 				>
-					{/* {SignUser ? '    list goes here' : '   need to sign in'} */}
-          list goes here
+					{SignUser ? '    list goes here' : (<> <div> Recently Loved</div> <div>Use your Loves list to keep track of your favorite products.</div></>)}
 				</div>
 			)}
 		</div>
