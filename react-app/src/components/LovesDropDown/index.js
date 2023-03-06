f4d7186bc1c42adb0c1cbb76c6eb98c772b9a355
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-03-05 18:53:19 -0800

    Seed database for Heroku

--- react-app/src/components/LovesDropDown/index.js
+++ react-app/src/components/LovesDropDown/index.js
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BsSuitHeart, BsSuitHeartFill } from 'react-icons/bs';
-
+// import SignUpModal from '../SignUpModal'
+// import LoginModal from '../LoginModal'
 import './index.css';
 const LovesDropDown = () => {
 	const dispatch = useDispatch();
-	const loves = useSelector(state => state.loves.loves);
-  console.log('this is loves', loves)
+	// const loves = useSelector(state => Object.keys(state.loves.loves));
+	// console.log('this is loves', loves)
 	const [dropdown, setDropdown] = useState(false);
 	// const userloves = useSelector((state) => Object.values(state.loves.loves));
 	// NOTE vvv why tf is this not working vvv
-	const SignUser = useSelector(state => state.session.user);
+	const SignUser = useSelector((state) => state.session.user);
 	// console.log(SignUser)
-// <div className='loves-dropdown-heart'><BsSuitHeartFill /></div>
+	// <div className='loves-dropdown-heart'><BsSuitHeartFill /></div>
 	return (
 		<div
 			className='loves-dropdown-container'
 			onMouseOver={() => setDropdown(true)}
-			onMouseLeave={() => setDropdown(true)}
+			onMouseLeave={() => setDropdown(false)}
 		>
 			<button className='loves-dropdown-button'>
-
-				{dropdown ? (<BsSuitHeartFill />) : (<BsSuitHeart />)}
-
+				{dropdown ? <BsSuitHeartFill /> : <BsSuitHeart />}
 			</button>
 
 			{dropdown && (
@@ -30,7 +29,18 @@ const LovesDropDown = () => {
 					className='loves-dropdown-page'
 					style={{ transition: 'all .8s linear 0s' }}
 				>
-					{SignUser ? '    list goes here' : (<> <div> Recently Loved</div> <div>Use your Loves list to keep track of your favorite products.</div></>)}
+					{SignUser ? (
+						'    list goes here'
+					) : (
+						<>
+							{' '}
+							<div> Recently Loved</div>{' '}
+							<div>
+								Use your Loves list to keep track of your favorite products.
+							</div>{' '}
+							<div> </div>{' '}
+						</>
+					)}
 				</div>
 			)}
 		</div>
