c45c0cbab31bcb3394a36c25eca7c422e23cf9bc
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-03-09 23:00:29 -0800

    add event handler to listen for 'enter' button

--- react-app/src/components/SearchBar/index.js
+++ react-app/src/components/SearchBar/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './SearchBar.css';
-import {BiSearchAlt} from 'react-icons/bi'
+
 const SearchBar = () => {
 	const [searchQuery, setSearchQuery] = useState('');
 	const [results, setResults] = useState([]);
+  const [dropdown, setDropDown] = useState(false)
 	console.log('search query', searchQuery);
 	console.log('results', results);
 
@@ -16,35 +17,46 @@ const SearchBar = () => {
 		try {
 			const res = await fetch(`/api/products/search?query=${searchQuery}`);
 			const data = await res.json();
+      setDropDown(true)
 			setResults(data.retrieve_products);
 		} catch (err) {
 			console.error(err);
 		}
 	};
+  const handleKeyPress = (e) =>{
+    if(e.keyCode === 'Enter'){
+      handleSearchQuery()
+    }
+  }
+  
 	return (
-		<div className='search-bar-container'>
-			<form onSubmit={handleSearchQuery}>
+		<form  className='search-bar-container' onSubmit={handleSearchQuery}>
+			<div>
 				<div className='search-bar-form'>
 					<input
-          className='search-input'
-						type='text'
-						// name="search"
+						className='search-input'
+						type='search'
 						value={searchQuery}
 						onChange={(e) => setSearchQuery(e.target.value)}
 						placeholder='Search'
-					/>
-					<button type='submit'>
-          {/* <div class='bx bx-search-alt' style={{size:'20px', backgroundColor:'transparent'}}></div> */}
-          <div className='search-icon'>
-          <box-icon name='search-alt' size='sm' border='none' pull='left' color='black'/>
-          </div>
-           </button>
-					{searchQuery.length ? (
-						<div className='results-dropdown'>
-							{results.map((product) => (
-								<div className='result-name font-12' key={product.id} value={product.id}>
-									<NavLink to={`products/${product.id}`}> {product.product_name}</NavLink>
+            onKeyDown={handleKeyPress}
 
+					/>
+
+					{searchQuery.length ? (
+						<div className='results-dropdown'
+
+            >
+							{results.map((product) => (
+								<div
+									className='result-name font-12'
+									key={product.id}
+									value={product.id}
+
+								>
+									<NavLink to={`products/${product.id}`}>
+										{product.product_name}
+									</NavLink>
 								</div>
 							))}
 						</div>
@@ -52,8 +64,8 @@ const SearchBar = () => {
 						<div></div>
 					)}
 				</div>
-			</form>
-		</div>
+			</div>
+		</form>
 	);
 };
 
