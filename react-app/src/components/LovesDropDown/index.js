32638a24d3fa59e5d4319d5405b532e17e69e6eb
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-04-27 14:43:18 -0700

    Add media query for smaller screen on checkout dropdown menu component and loves dropdown menu component

--- react-app/src/components/LovesDropDown/index.js
+++ react-app/src/components/LovesDropDown/index.js
@@ -29,120 +29,120 @@ const LovesDropDown = () => {
 	};
 	return (
 		<div
-			className='loves-dropdown-container'
+			className="loves-dropdown-container"
 			onMouseOver={() => setDropdown(true)}
-			onMouseLeave={() => setDropdown(true)}
+			onMouseLeave={() => setDropdown(false)}
 		>
-			<div className='loves-dropdown-button-container'>
-				<button className='loves-dropdown-button'>
-					{dropdown ? (<BsSuitHeartFill />) : (<BsSuitHeart />)}
+			<div className="loves-dropdown-button-container">
+				<button className="loves-dropdown-button">
+					{dropdown ? <BsSuitHeartFill /> : <BsSuitHeart />}
 				</button>
-			</div>
-			<div className='loves-dropdown-page-container'>
-				{dropdown ? (
-					<div
-						className='loves-dropdown-page'
-						style={{ transition: 'all .1s linear 0s' }}
-					>
-						{signedInUser ? (
-							<>
-								<div className='loves-dropdown-title font-20'>
-									Recently Loved
-									<NavLink
-										className='font-20 remove-underline'
-										to={`/loves/${signedInUser.id}`}
-									>
-										View all
-									</NavLink>
-								</div>
-								{userLoves.length ? (
-									<div className='loves-dropdown-list-container'>
-										{userLoves.map((love) => (
-											<>
-												<div className='love-row-container'>
-													<div className='love-left-container'>
-														<img
-															className='love-img'
-															src={love.prod_id.product_photos[0].prod_photo}
-														/>
-													</div>
-													<div className='love-middle-container font-12'>
-														<div style={{ fontWeight: 'bold' }}>
-															{love.prod_id.product_brand}
-														</div>
-														<div>{love.prod_id.product_name}</div>
-														<div style={{ fontWeight: 'bold' }}>
-															{love.prod_id.product_price}
-														</div>
-													</div>
-													<div className='love-right-container'>
-														{/* <AddToBasketButton style={{width:'10px'}}/> */}
-														<NavLink
-															to={`/products/${love.prod_id.id}`}
-															className='remove-underline font-18'
-															style={{ fontWeight: 'bold' }}
-														>
-															View
-														</NavLink>
-														{/* <LoveButton/> */}
-													</div>
-												</div>
-											</>
-										))}
+				</div>
+				<div className="loves-dropdown-page-container">
+					{dropdown ? (
+						<div
+							className="loves-dropdown-page"
+							style={{ transition: 'all .1s linear 0s' }}
+						>
+							{signedInUser ? (
+								<>
+									<div className="loves-dropdown-title font-20">
+										Recently Loved
+										<NavLink
+											className="font-20 remove-underline"
+											to={`/loves/${signedInUser.id}`}
+										>
+											View all
+										</NavLink>
 									</div>
-								) : (
-									<>
-										<div className='loves-dropdown-subtext font-14'>
-											Use your Loves list to keep track of your favorite
-											products.
+									{userLoves.length ? (
+										<div className="loves-dropdown-list-container">
+											{userLoves.map((love) => (
+												<>
+													<div className="love-row-container">
+														<div className="love-left-container">
+															<img
+																className="love-img"
+																src={love.prod_id.product_photos[0].prod_photo}
+															/>
+														</div>
+														<div className="love-middle-container font-12">
+															<div style={{ fontWeight: 'bold' }}>
+																{love.prod_id.product_brand}
+															</div>
+															<div>{love.prod_id.product_name}</div>
+															<div style={{ fontWeight: 'bold' }}>
+																{love.prod_id.product_price}
+															</div>
+														</div>
+														<div className="love-right-container">
+															{/* <AddToBasketButton style={{width:'10px'}}/> */}
+															<NavLink
+																to={`/products/${love.prod_id.id}`}
+																className="remove-underline font-18"
+																style={{ fontWeight: 'bold' }}
+															>
+																View
+															</NavLink>
+															{/* <LoveButton/> */}
+														</div>
+													</div>
+												</>
+											))}
 										</div>
-										<div>
-											<NavLink
-												to={'/products/categories/Makeup'}
-												className='loves-dropdown-shop-now font-20-white'
-											>
-												Shop Now
-											</NavLink>
-										</div>
-									</>
-								)}
-							</>
-						) : (
-							<div>
-								<div className='loves-dropdown-title font-20 '>
-									Recently Loved
+									) : (
+										<>
+											<div className="loves-dropdown-subtext font-14">
+												Use your Loves list to keep track of your favorite
+												products.
+											</div>
+											<div>
+												<NavLink
+													to={'/products/categories/Makeup'}
+													className="loves-dropdown-shop-now font-20-white"
+												>
+													Shop Now
+												</NavLink>
+											</div>
+										</>
+									)}
+								</>
+							) : (
+								<div>
+									<div className="loves-dropdown-title font-20 ">
+										Recently Loved
+									</div>
+									<div className="loves-dropdown-subtext font-14">
+										Use your Loves list to keep track of your favorite products.
+									</div>
+									<div className="not-signed-button-container">
+										<button
+											className="profile-signin font-16-white"
+											onClick={closeForLogin}
+										>
+											Sign in
+										</button>
+										<button
+											className="profile-login font-16"
+											onClick={closeForSignUp}
+										>
+											Create Account
+										</button>
+									</div>
 								</div>
-								<div className='loves-dropdown-subtext font-14'>
-									Use your Loves list to keep track of your favorite products.
-								</div>
-								<div className='not-signed-button-container'>
-									<button
-										className='profile-signin font-16-white'
-										onClick={closeForLogin}
-									>
-										Sign in
-									</button>
-									<button
-										className='profile-login font-16'
-										onClick={closeForSignUp}
-									>
-										Create Account
-									</button>
-								</div>
-							</div>
-						)}
-					</div>
-				) : (
-					<div
-						style={{
-							visibility: 'hidden',
-							opacity: '0',
-							transform: 'translateY(8px)',
-							transition: 'all 0.1s ease 0s;',
-						}}
-					></div>
-				)}
-			</div>
+							)}
+						</div>
+					) : (
+						<div
+							style={{
+								visibility: 'hidden',
+								opacity: '0',
+								transform: 'translateY(8px)',
+								transition: 'all 0.1s ease 0s;',
+							}}
+						></div>
+					)}
+				</div>
 
 			<div>
 				{showSignUpModal && (
@@ -150,7 +150,6 @@ const LovesDropDown = () => {
 						<SignUpModal setShowSignUpModal={setShowSignUpModal} />
 					</Modal>
 				)}
-
 			</div>
 			<div>
 				{showLoginModal && (
